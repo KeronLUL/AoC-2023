a89200e2e3f901772a38b2b959a6dc9bd4f1321f
TheKeron <xkeron19@gmail.com>
2023-12-06 17:02:56 +0100

    Add typedef

--- src/day03.cpp
+++ src/day03.cpp
@@ -3,8 +3,11 @@
 #include <tuple>
 #include <regex>
 
-void check_symbol(std::vector<std::tuple<int, char, int>> symbols, std::tuple<int, int, int> tuple,
-                  std::map<std::pair<size_t, size_t>, std::tuple<size_t, size_t>> *map, size_t *sum_part1){
+typedef std::vector<std::tuple<int, char, int>> vtChar;
+typedef std::vector<std::tuple<int, int, int>> vtInt;
+typedef std::map<std::pair<size_t, size_t>, std::tuple<size_t, size_t>> ptMap;
+
+void check_symbol(vtChar symbols, std::tuple<int, int, int> tuple, ptMap *map, size_t *sum_part1){
     for (size_t i = 0; i < symbols.size(); i++){
         if (std::get<0>(symbols[i]) >= std::get<1>(tuple) && std::get<0>(symbols[i]) <= std::get<2>(tuple) ) {
             *sum_part1 += std::get<0>(tuple);
@@ -23,15 +26,15 @@ void check_symbol(std::vector<std::tuple<int, char, int>> symbols, std::tuple<in
 
 void solution(std::istream &input, size_t *sum_part1, size_t *sum_part2){
     std::string line;
-    std::vector<std::vector<std::tuple<int, char, int>>> symbols;
-    std::vector<std::vector<std::tuple<int, int, int>>> numbers;
+    std::vector<vtChar> symbols;
+    std::vector<vtInt> numbers;
     size_t line_count = 0;
 
     while (std::getline(input, line)){
         std::string symbols_line = line;
         std::string numbers_line = line;
-        std::vector<std::tuple<int, char, int>> line_symbols;
-        std::vector<std::tuple<int, int, int>> line_numbers;
+        vtChar line_symbols;
+        vtInt line_numbers;
         std::smatch match;
         size_t pos_sym = 0;
         size_t pos_num = 0;
@@ -53,7 +56,7 @@ void solution(std::istream &input, size_t *sum_part1, size_t *sum_part2){
         numbers.push_back(line_numbers);
     }
 
-    std::map<std::pair<size_t, size_t>, std::tuple<size_t, size_t>> map;
+    ptMap map;
     for (size_t i = 0; i < numbers.size(); i++){
         for ( auto tuple : numbers[i]) {
             check_symbol(symbols[i], tuple, &map, sum_part1);
